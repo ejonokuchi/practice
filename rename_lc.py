f48db85d7e3b56d82b85b6ad409dfb26d2b1ee34
evan <ejonokuchi@gmail.com>
2019-06-17 23:53:41 -0700

    better naming and solved candy crush

--- rename_lc.py
+++ rename_lc.py
@@ -11,23 +11,25 @@ import re
 
 def rename_leetcode_files():
     """
-    Renames all leetcode files in this directory.
+    Renames the default name of leetcode files in this directory.
 
     """
     EXCLUDED = [
         'README.md',
         'rename_lc.py',
     ]
-    lc_file_re = re.compile(r'(?P<id>[\d]+)(?P<filename>\.[\w\-]+.py)')
+    lc_file_re = re.compile(r'^(?P<id>[\d]+)\.(?P<filename>[\w\-]+).py$')
     files = [f for f in os.listdir() if f not in EXCLUDED and f[0] != '.']
 
     for f in files:
         match = lc_file_re.match(f)
         if match is not None:
             d = match.groupdict()
-            new_filename = f"{int(d['id']):>04d}{d['filename']}"
-            print(f'Renaming "{f}" to "{new_filename}"')
-            os.rename(f, new_filename)
+            new_filename = d['filename'].replace('-', '_')
+            new_path = f"{int(d['id']):>04d}-{new_filename}.py"
+            # print(f'Would rename "{f}" to "{new_path}"...')
+            print(f'Renaming "{f}" to "{new_path}"')
+            os.rename(f, new_path)
     return
 
 

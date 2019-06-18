3ab203aef6f097dca5b81c59b4bd7b6816a5c6cf
evan <ejonokuchi@gmail.com>
2019-06-17 17:43:10 -0700

    adding first problem group

---
+++ rename_lc.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+
+def rename_leetcode_files():
+    """
+    Renames all leetcode files in this directory.
+
+    """
+    EXCLUDED = [
+        'README.md',
+        'rename_lc.py',
+    ]
+    lc_file_re = re.compile(r'(?P<id>[\d]+)(?P<filename>\.[\w\-]+.py)')
+    files = [f for f in os.listdir() if f not in EXCLUDED and f[0] != '.']
+
+    for f in files:
+        match = lc_file_re.match(f)
+        if match is not None:
+            d = match.groupdict()
+            new_filename = f"{int(d['id']):>04d}{d['filename']}"
+            print(f'Renaming "{f}" to "{new_filename}"')
+            os.rename(f, new_filename)
+    return
+
+
+if __name__ == '__main__':
+    rename_leetcode_files()

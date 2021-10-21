a4a8d101a467d45c5c745eb494917446748addbc
Evan <ejonokuchi@gmail.com>
2021-10-20 22:09:06 -0600

    Add stubs for A* and Rabin-Karp

---
+++ algorithms/a_star.py
@@ -0,0 +1,9 @@
+"""
+A* Search
+---------
+Efficient graph search.
+
+See https://en.wikipedia.org/wiki/A*_search_algorithm
+
+"""
+
---
+++ algorithms/rabin_karp.py
@@ -0,0 +1,20 @@
+"""
+Rabin-Karp Substring Search
+---------------------------
+Efficient substring search.
+
+See https://en.wikipedia.org/wiki/Rabin–Karp_algorithm
+
+"""
+
+
+def rabin_karp_substring_search(S: str, pattern: str) -> int:
+    """
+    Returns the index of the pattern in the string to search, S. If the pattern is not
+    found, returns -1.
+
+    Time  :
+    Space :
+    """
+    pass
+

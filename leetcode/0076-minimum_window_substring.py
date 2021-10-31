15fe16c71e3b1dec308f30ded84504c42828e599
Evan <ejonokuchi@gmail.com>
2021-10-30 20:19:47 -0700

    Solve 76

--- leetcode/0076-minimum_window_substring.py
+++ leetcode/0076-minimum_window_substring.py
@@ -61,7 +61,47 @@
 # Follow up: Could you find an algorithm that runs in O(m + n) time?
 #
 
+from collections import Counter
+
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        pass
+        """
+        Creates a dictionary of required character counts and slides a window over s,
+        updating counts as characters are added and removed.
+
+        Counts can be negative, indicating the window contains an extra copy of a given
+        character. When no required characters are missing, the window is valid.
+
+        Time  : O(m + n)
+        Space : O(n)
+        """
+        # Create dictionary of required character counts
+        counts = Counter(t)
+        num_missing = len(t)
+
+        # Slide a window (l, r) over string s
+        start, end = 0, 0
+        l, r = 0, -1
+        while r < len(s) - 1:
+            # Shift right pointer, until the window is valid or the end of s
+            while r < len(s) - 1 and num_missing > 0:
+                r += 1
+                if counts[s[r]] > 0:
+                    num_missing -= 1
+                counts[s[r]] -= 1
+            if num_missing > 0:
+                break
+            # Shift left pointer, until the leftmost character is required
+            while counts[s[l]] < 0:
+                counts[s[l]] += 1
+                l += 1
+            # Update minimum window, if applicable
+            if end == 0 or r - l + 1 < end - start:
+                start, end = l, r + 1
+            # Increment left pointer for next iteration
+            num_missing += 1
+            counts[s[l]] += 1
+            l += 1
+
+        return s[start:end]

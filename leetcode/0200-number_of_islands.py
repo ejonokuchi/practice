709cfbe2fd3b3380105f6733b48ff87a50828cef
Evan <ejonokuchi@gmail.com>
2021-10-31 23:31:12 -0700

    Solve 200

--- leetcode/0200-number_of_islands.py
+++ leetcode/0200-number_of_islands.py
@@ -60,4 +60,28 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        pass
+        """
+        Runs depth-first search from each unvisited land cell to label all connected
+        cells. The grid is modified in-place to mark each cell as visited.
+
+        Time  : O(nm)
+        Space : O(nm)
+        """
+        n, m = len(grid), len(grid[0])
+
+        def label_connected(r: int, c: int):
+            """Label all connected land cells with the current component index, via
+            recursive DFS."""
+            if 0 <= r < n and 0 <= c < m and grid[r][c] == "1":
+                grid[r][c] = component_idx
+                for i, j in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
+                    label_connected(i, j)
+            return
+
+        component_idx = 0
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == "1":
+                    label_connected(r, c)
+                    component_idx += 1
+        return component_idx

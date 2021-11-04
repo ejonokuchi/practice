4b3229e7a823100628f9ee9e7ef98bf840fec8cb
Evan <ejonokuchi@gmail.com>
2021-11-04 09:51:34 -0700

    Add quickselect, ignore in-place

--- algorithms/quickselect.py
+++ algorithms/quickselect.py
@@ -6,6 +6,7 @@ Given a list of integers, find the kth lowest element (0 < k <= n).
 """
 
 import random
+from collections import deque
 from typing import List
 
 
@@ -13,31 +14,48 @@ def quickselect(A: List[int], k: int) -> int:
     """
     Returns an element of rank k from an unsorted list.
 
-    Time  :
-    Space :
+    Stable selection algorithm.
+
+    Time  : O(n) average, O(n^2) worst
+    Space : O(n)
     """
-    pass
+    # Pivot selection
+    # Use median-of-three to avoid worst-case where pivot is the min or max.
+    if len(A) >= 3:
+        idx_sample = random.sample(range(len(A)), 3)
+        pivot = sorted(A[idx] for idx in idx_sample)[1]
+    else:
+        pivot = A[0]
+
+    # Partition
+    # Use 3 arrays instead of 2 to cut recursion depth for inputs with many repeats.
+    L, M, R = deque([]), deque([]), deque([])
+    for x in A:
+        if x < pivot:
+            L.append(x)
+        elif x == pivot:
+            M.append(x)
+        else:
+            R.append(x)
+
+    # Recurse
+    if k <= len(L):
+        return quickselect(list(L), k)
+    elif k <= (len(L) + len(M)):
+        return M[k - len(L) - 1]  # stability condition
+    else:
+        return quickselect(list(R), k - len(L) - len(M))
 
 
-def quickselect_inplace(A: List[int], k: int) -> int:
-    """
-    Returns an element of rank k from an unsorted list, in-place (constant memory).
-
-    Time  :
-    Space :
-    """
-    pass
+def test_quickselect_few_repeats():
+    for _ in range(100):
+        A = [random.randint(0, 100000) for _ in range(100)]
+        k = 10
+        assert quickselect(A, k) == sorted(A)[k - 1]
 
 
-def test_quickselect_1():
-    A = [random.randint(0, 100000) for _ in range(1000)]
-    k = 514
-    assert quickselect(A, k) == sorted(A)[k - 1]
-    assert quickselect_inplace(A, k) == sorted(A)[k - 1]
-
-
-def test_quickselect_2():
-    A = [random.randint(0, 100000) for _ in range(1000)]
-    k = 10
-    assert quickselect(A, k) == sorted(A)[k - 1]
-    assert quickselect_inplace(A, k) == sorted(A)[k - 1]
+def test_quickselect_many_repeats():
+    for _ in range(100):
+        A = [random.randint(0, 1000) for _ in range(1000)]
+        k = 514
+        assert quickselect(A, k) == sorted(A)[k - 1]

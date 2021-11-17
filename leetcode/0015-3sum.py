1a7faea78ef799f783fe7829c8cc838fbe102532
Evan <ejonokuchi@gmail.com>
2021-11-16 20:46:22 -0800

    Solve 15

--- leetcode/0015-3sum.py
+++ leetcode/0015-3sum.py
@@ -43,4 +43,60 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass
+        """
+        Computes valid triplets by repeatedly solving the 2-sum problem over the sorted
+        input array.
+
+        For each element x at index i, set up two pointers, j and k, at i + 1 and n - 1.
+        Check the three-sum for this triplet. If it's too low, slide the left pointer to
+        the middle. If it's too high, slide the right pointer to the middle.
+
+        Time  : O(n^2)
+        Space : O(n)
+
+        Examples
+        --------
+        Given:
+        sorted(nums) = [-2 -1 0 2 5]
+
+        [-2 -1 0 2 5]
+          i  j     k
+
+        --> -2 + -1 + 5 = 2
+        --> 2 is too high, k-- will decrease the sum.
+
+        [-2 -1 0 2 5]
+          i  j   k
+
+        --> -2 + -1 + 2 = -1
+        --> -1 is too low, j++ will increase the sum.
+
+        [-2 -1 0 2 5]
+          i    j k
+
+        --> -2 + 0 + 2 = 0
+        --> Sum is 0, save this triplet. Since j now crosses k, all solutions including
+        nums[i] have now been considered.
+
+        """
+        n = len(nums)
+        nums.sort()
+        solutions = set()
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:  # skip duplicates
+                continue
+            # Solve two-sum for target of nums[i].
+            target = -nums[i]
+            j, k = i + 1, n - 1
+            while j < k:
+                if nums[j] + nums[k] < target:
+                    j += 1
+                elif nums[j] + nums[k] > target:
+                    k -= 1
+                else:
+                    solutions.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+                    while j < k and nums[j] == nums[j + 1]:  # skip duplicates
+                        j += 1
+                    j += 1
+
+        return [list(x) for x in solutions]

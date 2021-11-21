6d0bbda401ad97dba0729440817d540c89fc69da
Evan <ejonokuchi@gmail.com>
2021-11-20 20:42:00 -0800

    solve 5

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Software Engineer and Machine Learning Interview resources, for personal use.
 
 ### Leetcode Problems
 
-70/75.
+71/75.
 
 Arrays: ✅✅✅✅✅✅✅✅✅✅
 - [x] [1. Two Sum (Easy)](https://leetcode.com/problems/two-sum/)
@@ -73,7 +73,7 @@ Matrix: ✅✅✅✅
 - [x] [48. Rotate Image (Medium)](https://leetcode.com/problems/rotate-image/)
 - [x] [79. Word Search (Medium)](https://leetcode.com/problems/word-search/)
 
-String: ✅✅✅✅✅✅✅⬜️⬜️⬜️
+String: ✅✅✅✅✅✅✅✅⬜️⬜️
 - [x] [3. Longest Substring Without Repeating Characters (Medium)](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
 - [x] [424. Longest Repeating Character Replacement (Medium)](https://leetcode.com/problems/longest-repeating-character-replacement/)
 - [x] [76. Minimum Window Substring (Hard)](https://leetcode.com/problems/minimum-window-substring/)
@@ -81,7 +81,7 @@ String: ✅✅✅✅✅✅✅⬜️⬜️⬜️
 - [x] [49. Group Anagrams (Medium)](https://leetcode.com/problems/group-anagrams/)
 - [x] [20. Valid Parentheses (Easy)](https://leetcode.com/problems/valid-parentheses/)
 - [x] [125. Valid Palindrome (Easy)](https://leetcode.com/problems/valid-palindrome/)
-- [ ] [5. Longest Palindromic Substring (Medium)](https://leetcode.com/problems/longest-palindromic-substring/)
+- [x] [5. Longest Palindromic Substring (Medium)](https://leetcode.com/problems/longest-palindromic-substring/)
 - [ ] [647. Palindromic Substrings (Medium)](https://leetcode.com/problems/palindromic-substrings/)
 - [ ] [271. Encode and Decode Strings - Premium (Medium)](https://leetcode.com/problems/encode-and-decode-strings/)
 
--- leetcode/0005-longest_palindromic_substring.py
+++ leetcode/0005-longest_palindromic_substring.py
@@ -56,4 +56,58 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        """
+        Computes the length of the longest palindrome centered at each index, and
+        returns the substring corresponding to the longest one.
+
+        From each index, checks for repeating characters, and sends two pointers left
+        and right until their characters no longer match or the end of the string is
+        reached.
+
+        Time  : O(n^2)
+        Space : O(1)
+
+        Example
+        -------
+        Given s = "raceecars"
+
+        idx = 0
+        (1) -> base window is from (0, 0): "r"
+        (2) -> window expands to: "r"
+        (3) -> not longer than current max (base case of first letter)
+
+        ... 1 and 2 are the same
+
+        idx = 3
+        (1) -> base window is from (3, 4): "ee"
+        (2) -> window expands to: "raceecar"
+        (3) -> longer than current max, update longest
+        --> this palindrome was centered on "ee", so we can skip idx = 4
+
+        idx = 5
+        (1) -> base window is from (5, 5): "c"
+        (2) -> window expands to: "c"
+        (3) -> not longer than current max
+
+        ... and so on.
+
+        """
+        n = len(s)
+        a, b, longest = 0, 1, 1
+        idx = 0
+        while idx < n:
+            i = j = idx
+            # (1) Find the "base window" including any repeated characters
+            while j < n - 1 and s[j + 1] == s[idx]:
+                j += 1
+            num_repeated = j - idx
+            # (2) Expand the window while the next characters match
+            while 0 < i and j < n - 1 and s[i - 1] == s[j + 1]:
+                i -= 1
+                j += 1
+            # (3) Compare to the longest palindrome thus far
+            if j - i + 1 > longest:
+                longest = j - i + 1
+                a, b = i, j + 1
+            idx = idx + 1 + num_repeated  # no need to check indices of the base window
+        return s[a:b]

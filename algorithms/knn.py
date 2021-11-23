bb71c38193a0313fcee62df2d2498ebeec3aa3a9
Evan <ejonokuchi@gmail.com>
2021-11-23 10:09:37 -0800

    Update KNN to use numpy

--- algorithms/knn.py
+++ algorithms/knn.py
@@ -3,19 +3,14 @@ K-nearest neighbors
 -------------------
 Classifies new observations based on the majority class of the k-nearest neighbors.
 
-Standard libraries only.
+Standard libraries and numpy only.
 
 """
 
 import heapq
-import math
 from collections import Counter
-from typing import List
 
-
-def compute_distance(A: List[int], B: List[int]) -> List[int]:
-    """Compute the Euclidean distance between A and B."""
-    return math.sqrt(sum((a - b) ** 2 for a, b in zip(A, B)))
+import numpy as np
 
 
 class KNeighborsClassifier:
@@ -23,7 +18,7 @@ class KNeighborsClassifier:
     Given a set of observations with known classes, classify a new observation based on
     the classes of the nearest k observations.
 
-    Uses a min-heap to find the k smallest distances over all observations, and returns
+    Uses a max-heap to find the k smallest distances over all observations, and returns
     the class with the maximum count.
 
     Time  : O(n log k)
@@ -40,35 +35,46 @@ class KNeighborsClassifier:
     def __init__(self, k: int = 3):
         self.k = k
 
-    def train(self, X: List[List[int]], Y: List[int]):
+    def train(self, X: np.ndarray, Y: np.ndarray):
+        """Records the input data to compute distances for inference."""
+        self.n, self.m = X.shape
+        assert Y.shape == (self.n,)
         self.X = X
         self.Y = Y
 
-    def classify(self, x: List[int]) -> int:
-        distances = [compute_distance(x, neighbor) for neighbor in self.X]
-        k_neighbor_heap = []
-        for idx, d in enumerate(distances):
-            if idx < self.k:
-                heapq.heappush(k_neighbor_heap, (-d, idx))
-            else:
-                heapq.heappushpop(k_neighbor_heap, (-d, idx))
+    def classify(self, x: np.ndarray) -> int:
+        """Computes distances to all points, uses a max-heap to find the k smallest
+        distances, and returns the majority class.
+
+        Time  : O(n log k)
+        Space : O(n)
+        """
+        assert x.shape == (self.m,)
+
+        distances = np.linalg.norm(self.X - x, axis=1)
+        k_neighbor_heap = [(-d, idx) for idx, d in enumerate(distances[: self.k])]
+        heapq.heapify(k_neighbor_heap)
+        for idx, d in enumerate(distances[self.k :], self.k):
+            heapq.heappushpop(k_neighbor_heap, (-d, idx))
 
         k_distances, k_neighbor_idxs = zip(*k_neighbor_heap)
-        k_neighbor_classes = [self.Y[idx] for idx in k_neighbor_idxs]
+        k_neighbor_classes = self.Y[list(k_neighbor_idxs)]
         return Counter(k_neighbor_classes).most_common(1)[0][0]
 
 
 def test_knn():
-    X = [
-        [7, 7],
-        [7, 4],
-        [3, 4],
-        [1, 4],
-    ]
-    Y = [0, 0, 1, 1]
+    X = np.array(
+        [
+            [7, 7],
+            [7, 4],
+            [3, 4],
+            [1, 4],
+        ]
+    )
+    Y = np.array([0, 0, 1, 1])
 
     k = 3
-    x = [3, 7]
+    x = np.array([3, 7])
     y = 1
     model = KNeighborsClassifier(k=k)
     model.train(X, Y)
